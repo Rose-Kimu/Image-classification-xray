f25cfcc34ca79af32e5080a697ec8942551703a7
Rose Kimu <Rose Kimu>
2024-03-31 15:50:39 +0300

    push

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ async def predict(file: UploadFile = File(...)):
 async def read_root():
     return {"message": "Welcome to the Image Classification API"}
     
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
